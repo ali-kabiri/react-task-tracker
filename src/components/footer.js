d6a7542db1e33b27b844c956c9540566088fd601
System Administrator <root@ALIs-MacBook-Pro.local>
2023-12-09 12:09:49 +0330

    Add Styling to index.css

---
+++ src/components/footer.js
@@ -0,0 +1,8 @@
+//write react function to get user login incliding email and password 
+function getUserLogin(email, password) {
+    // code for getting user login
+    const userLogin = getUserLogin(email, password);
+    console.log(userLogin);
+}
+
+
